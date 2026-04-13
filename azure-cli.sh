c3a50ad56501ba6ad7e0eb0e250bda757fc2c90f
Satyam Maurya <satyammaurya1001@gmail.com>
2026-04-14 00:16:24 +0530

    Update azure-cli.sh

--- azure-cli.sh
+++ azure-cli.sh
@@ -1,18 +1,15 @@
-#STEP-1: Installing Azure CLI
+#STEP-1: Installing Azure CLI in local windows machine
 
 winget install Microsoft.AzureCLI
 winget install --exact --id Microsoft.AzureCLI
 
-#Login Method-2: provide azure login
+#Verify-2: login and verify
 
 az login --use-device-code #[Open the Link on the browser and give code]
-
-#Verify-3:
+az --version
 az account show
 
-#STEP-4: to Install Azure CLI insilde ubuntu VM to access storage account
+#STEP-3: to Install Azure CLI insilde ubuntu VM to access storage account
 
 sudo apt update && sudo apt upgrade -y
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-az --version
-#az login --use-device-code   [Open the Link on the browser and give code]
